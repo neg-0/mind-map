e0fac12c86657a12665e6f9e06d173fbf46231f3
Dustin Stringer <dustin.stringer@us.af.mil>
2023-04-18 21:11:28 -0400

    added some default text

--- src/initialNodes.tsx
+++ src/initialNodes.tsx
@@ -7,9 +7,12 @@ export default [
     position: { x: 100, y: 200 },
     data: {
       textFields: {
-        "handle-0": "smoothstep",
-        "handle-1": "smoothstep",
-        "handle-2": "smoothstep",
+        "text-0":
+          "In today's rapidly evolving world, technology plays a crucial role in addressing environmental challenges and promoting mental well-being. Innovations such as renewable energy sources, eco-friendly practices, and smart devices have the potential to reduce our environmental footprint and improve the quality of life for individuals across the globe. By harnessing these advancements, businesses can contribute to sustainable development and create a healthier future for all.",
+        "text-1":
+          "One of the key areas where technology can make a significant impact is environmental sustainability. By implementing green technologies, such as solar panels, energy-efficient appliances, and electric vehicles, individuals and organizations can reduce their carbon emissions and conserve natural resources. In addition, digital tools can help monitor and manage energy consumption, waste reduction, and recycling efforts, leading to more informed decisions and sustainable practices.",
+        "text-2":
+          "Technology also offers numerous opportunities to enhance mental health and well-being. For instance, the rise of telemedicine and online therapy platforms has made mental health care more accessible and convenient for people in need. Furthermore, mobile applications and wearable devices can help individuals monitor their mental health, track their emotions, and develop self-care strategies. By integrating technology into our daily lives, we can foster a supportive environment that encourages open conversations about mental health and creates a more empathetic and understanding society.",
       },
     },
   },
