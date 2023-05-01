9cda0db76af7846bc802c9b6d4ff27e653b3aa1a
Dustin Stringer <dustin.stringer@us.af.mil>
2023-04-30 22:03:08 -0400

    added store persistence (local)

--- src/Store.tsx
+++ src/Store.tsx
@@ -12,11 +12,12 @@ import {
   applyNodeChanges,
 } from "reactflow";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 import initialEdges from "./initialEdges";
 import initialNodes from "./initialNodes";
 
-type RFState = {
+type NodeState = {
   nodes: Node[];
   edges: Edge[];
   onNodesChange: OnNodesChange;
@@ -26,35 +27,42 @@ type RFState = {
 };
 
 // this is our useStore hook that we can use in our components to get parts of the store and call actions
-const useStore = create<RFState>((set, get) => ({
-  nodes: initialNodes,
-  edges: initialEdges,
-  onNodesChange: (changes: NodeChange[]) => {
-    set({
-      nodes: applyNodeChanges(changes, get().nodes),
-    });
-  },
-  onEdgesChange: (changes: EdgeChange[]) => {
-    set({
-      edges: applyEdgeChanges(changes, get().edges),
-    });
-  },
-  onConnect: (connection: Connection) => {
-    set({
-      edges: addEdge(connection, get().edges),
-    });
-  },
-  updateTextContent: (nodeId: string, handleId: string, text: string) => {
-    set({
-      nodes: get().nodes.map((n) => {
-        if (n.id === nodeId) {
-          n.data = { ...n.data };
-          n.data.textFields[handleId] = text;
-        }
-        return n;
-      }),
-    });
-  },
-}));
+const useStore = create<NodeState>()(
+  persist(
+    (set, get) => ({
+      nodes: initialNodes,
+      edges: initialEdges,
+      onNodesChange: (changes: NodeChange[]) => {
+        set({
+          nodes: applyNodeChanges(changes, get().nodes),
+        });
+      },
+      onEdgesChange: (changes: EdgeChange[]) => {
+        set({
+          edges: applyEdgeChanges(changes, get().edges),
+        });
+      },
+      onConnect: (connection: Connection) => {
+        set({
+          edges: addEdge(connection, get().edges),
+        });
+      },
+      updateTextContent: (nodeId: string, handleId: string, text: string) => {
+        set({
+          nodes: get().nodes.map((n) => {
+            if (n.id === nodeId) {
+              n.data = { ...n.data };
+              n.data.textFields[handleId] = text;
+            }
+            return n;
+          }),
+        });
+      },
+    }),
+    {
+      name: "nodes",
+    }
+  )
+);
 
 export default useStore;
